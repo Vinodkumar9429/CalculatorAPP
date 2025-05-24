0e7c68edfd8822ed9d58b4f87d0e221248c5c7e0
Vinodkumar9429 <umr09347@gmail.com>
2025-05-24 11:59:02 +0530

    Every calculation funcitonality added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,33 @@
 import './style.css';
 
-const display = document.getElementById('display') as HTMLInputElement;
+const display = document.querySelector('#display') as HTMLInputElement | null;
+const operators = ['+', '-', '*', '/', '.'];
+document.querySelectorAll('.show').forEach((btn) =>{
+  btn.addEventListener('click', () => {
+    const val = (btn as HTMLButtonElement).textContent;
+
+    if (!display || val === null) return;
+
+    const lastChar = display.value.slice(-1);
+
+    if (operators.includes(val) && operators.includes(lastChar)) {
+      return;
+    }
+
+    display.value += val;
+  });
+});
+
+const clear = document.querySelector('.clear') as HTMLButtonElement;
+clear.addEventListener('click', ()=>{
+  if(display !== null){
+    display.value = '';
+  }
+})
+
+const equals = document.querySelector('.equals') as HTMLButtonElement;
+equals.addEventListener('click', () =>{
+  if(display !== null){
+    display.value = eval(display.value);
+  }
+})
